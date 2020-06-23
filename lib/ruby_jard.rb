5a0c2b69a36e37007e096f3e50f517940ab1224d
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-06-24 00:43:58 +0700

    Add existing dependency

--- lib/ruby_jard.rb
+++ lib/ruby_jard.rb
@@ -1,6 +1,35 @@
-require "ruby_jard/version"
+require 'pry'
+require 'byebug/core'
+require 'byebug/attacher'
+require 'tty-cursor'
+require 'tty-box'
+require 'tty-screen'
+
+require 'ruby_jard/commands/continue_command'
+require 'ruby_jard/commands/up_command'
+require 'ruby_jard/commands/down_command'
+require 'ruby_jard/commands/next_command'
+require 'ruby_jard/commands/step_command'
+require 'ruby_jard/commands/finish_command'
+
+require 'ruby_jard/repl_processor'
+require 'ruby_jard/server'
+
+require 'ruby_jard/screen_manager'
+
+require 'ruby_jard/session'
+require 'ruby_jard/version'
 
 module RubyJard
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.current_session
+    @current_session ||= RubyJard::Session.new
+  end
+end
+
+module Kernel
+  def jard
+    RubyJard.current_session.attach
+  end
 end
