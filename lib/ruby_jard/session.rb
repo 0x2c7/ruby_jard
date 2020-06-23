878ace9ba69243a5731542bb0539cec525983707
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-06-24 00:43:52 +0700

    Add central session class to control debugging flow of a program

---
+++ lib/ruby_jard/session.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+require 'forwardable'
+
+module RubyJard
+  class Session
+    attr_reader :screen_manager, :backtrace, :frame
+
+    def initialize(options = {})
+      @options = options
+
+      @backtrace = []
+      @frame = nil
+
+      @started = false
+      @screen_manager = RubyJard::ScreenManager.new(session: self)
+      @server = RubyJard::Server.new(options)
+    end
+
+    def start
+      return if started?
+
+      setup_pry
+      setup_byebug
+
+      @server.start
+      @screen_manager.start
+
+      @started = true
+    end
+
+    def started?
+      @started == true
+    end
+
+    def attach
+      start unless started?
+
+      Byebug.attach
+      Byebug.current_context.step_out(2, false)
+    end
+
+    def refresh
+      @backtrace = Byebug.current_context.backtrace
+      @frame = Byebug.current_context.frame
+      @screen_manager.refresh
+    end
+
+    private
+
+    def setup_pry
+      Pry::Prompt.add(
+        :jard,
+        'Custom promt for Pry'
+      ) do |context, _nesting, pry_instance, sep|
+        format(
+          '%<name>s(%<context>s)%<separator>s ',
+          in_count: pry_instance.input_ring.count,
+          name: :jard,
+          context: Pry.view_clip(context),
+          separator: sep
+        )
+      end
+      Pry.config.prompt = Pry::Prompt[:jard]
+      Pry.config.hooks = Pry::Hooks.new
+    end
+
+    def setup_byebug
+      Byebug::Setting[:autolist] = false
+      Byebug::Setting[:autoirb] = false
+      Byebug::Setting[:autopry] = false
+      Byebug::Context.processor = RubyJard::ReplProcessor
+    end
+  end
+end
