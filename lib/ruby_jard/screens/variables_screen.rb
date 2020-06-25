037ecc3833e58b55fbd13ebd610b895b7fcf352e
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-06-25 18:39:04 +0700

    Implement variables screen

--- lib/ruby_jard/screens/variables_screen.rb
+++ lib/ruby_jard/screens/variables_screen.rb
@@ -10,11 +10,102 @@ module RubyJard
           )
         )
 
-        @output.print TTY::Cursor.move_to(@col + 2, @row)
+        @output.print TTY::Cursor.move_to(@col + 1, @row)
         @output.print decorate_text
           .with_highlight(true)
-          .text(' Variables ', :bright_yellow)
+          .text('Variables ', :bright_yellow)
           .content
+
+        decorated_variables.each_with_index do |variable, index|
+          @output.print TTY::Cursor.move_to(@col + 1, @row + index + 1)
+          @output.print variable.content
+        end
+      end
+
+      private
+
+      def current_binding
+        RubyJard.current_session.frame._binding
+      end
+
+      def current_frame
+        RubyJard.current_session.frame
+      end
+
+      def current_frame_scope
+        RubyJard.current_session.backtrace[RubyJard.current_session.frame.pos][1]
+      end
+
+      def decorated_variables
+        variables = []
+        variables_hash = {}
+
+        current_frame.args.map do |(_kind, variable)|
+          next if variables_hash[variable]
+
+          variables << [:arg, variable, current_binding.local_variable_get(variable)]
+          variables_hash[variable] = true
+        end
+
+        sort_variables(current_binding.local_variables).map do |variable|
+          next if variables_hash[variable]
+
+          variables << [:loc, variable, current_binding.local_variable_get(variable)]
+          variables_hash[variable] = true
+        end
+
+        sort_variables(current_frame_scope.instance_variables).map do |variable|
+          next if variables_hash[variable]
+
+          variables << [:ins, variable, current_frame_scope.instance_variable_get(variable)]
+          variables_hash[variable] = true
+        end
+
+        variables.map do |kind, name, value|
+          decorated_variable(kind, name, value)
+        end
+      end
+
+      def decorated_variable(kind, name, value)
+        text =
+          decorate_text
+          .text(decoreated_kind(kind))
+          .text(' ')
+          .with_highlight(true)
+          .text(name.to_s, :bright_white)
+          .with_highlight(false)
+          .text(' = ')
+        text.text(decorated_value(text, value), :white)
+      end
+
+      def decorated_value(text, value)
+        length = @layout.width - 2 - text.length
+        value_inspect = value.inspect.to_s
+        value_inspect = value.to_s if value_inspect.length > length
+        value_inspect = value_inspect.first(length - 1) + '»' if value_inspect.length > length
+        value_inspect
+      end
+
+      def decoreated_kind(kind)
+        kind_color =
+          case kind
+          when :arg
+            :yellow
+          when :loc
+            :green
+          else
+            :white
+          end
+
+        decorate_text
+          .with_highlight(false)
+          .text(kind.to_s, kind_color)
+      end
+
+      def sort_variables(variables)
+        variables.sort do |a, b|
+          a.to_s <=> b.to_s
+        end
       end
     end
   end
