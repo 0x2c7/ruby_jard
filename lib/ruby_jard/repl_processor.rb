4b95ac5291b873b67537c36df7ec2c361ac286f5
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-07-04 22:58:07 +0700

    Remove finish command

--- lib/ruby_jard/repl_processor.rb
+++ lib/ruby_jard/repl_processor.rb
@@ -5,7 +5,6 @@ require 'ruby_jard/commands/up_command'
 require 'ruby_jard/commands/down_command'
 require 'ruby_jard/commands/next_command'
 require 'ruby_jard/commands/step_command'
-require 'ruby_jard/commands/finish_command'
 require 'ruby_jard/commands/frame_command'
 
 module RubyJard
@@ -108,13 +107,6 @@ module RubyJard
       Byebug::DownCommand.new(self, 'down 1').execute
 
       process_commands
-    end
-
-    def handle_finish_command(_pry_instance, _options)
-      RubyJard.current_session.disable
-      context.step_out(2, true)
-      Byebug::NextCommand.new(self, 'next').execute
-      RubyJard.current_session.enable
     end
 
     def handle_continue_command(_pry_instance, _options)
