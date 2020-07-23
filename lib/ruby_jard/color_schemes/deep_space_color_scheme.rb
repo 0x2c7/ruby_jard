7deaa4bcedb1af6414d7f7e7822d33527ad99b37
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-07-23 17:07:59 +0700

    Increase brightness of colors

--- lib/ruby_jard/color_schemes/deep_space_color_scheme.rb
+++ lib/ruby_jard/color_schemes/deep_space_color_scheme.rb
@@ -9,43 +9,44 @@ module RubyJard
       GRAY3  = '#323c4d'
       GRAY4  = '#51617d'
       GRAY5  = '#9aa7bd'
+      WHITE  = '#fff'
       RED    = '#b15e7c'
-      GREEN  = '#709d6c'
-      YELLOW = '#b5a262'
-      BLUE   = '#608cc3'
-      PURPLE = '#8f72bf'
+      GREEN  = '#96f08d'
+      YELLOW = '#e8cb6b'
+      BLUE   = '#78b5ff'
+      PURPLE = '#b08aed'
       CYAN   = '#56adb7'
-      ORANGE = '#b3785d'
+      ORANGE = '#f28d5e'
       PINK   = '#c47ebd'
 
       BACKGROUND = GRAY1
       STYLES = {
-        background:                     [GRAY5, BACKGROUND],
+        background:                     [WHITE, BACKGROUND],
         screen_border:                  [GRAY3, BACKGROUND],
         screen_title:                   [GRAY2, BLUE],
         screen_title_highlighted:       [GRAY2, ORANGE],
-        screen_title_secondary:         [GRAY5, GRAY3],
-        screen_title_background:        [GRAY5, GRAY2],
+        screen_title_secondary:         [WHITE, GRAY3],
+        screen_title_background:        [WHITE, GRAY2],
         control_buttons:                [GRAY4, BACKGROUND],
         thread_id:                      [BLUE, BACKGROUND],
-        thread_name:                    [GRAY5, BACKGROUND],
+        thread_name:                    [WHITE, BACKGROUND],
         thread_status_run:              [GREEN, BACKGROUND],
         thread_status_sleep:            [GRAY4, BACKGROUND],
         thread_status_other:            [GRAY4, BACKGROUND],
-        thread_location:                [GRAY4, BACKGROUND],
+        thread_location:                [GRAY5, BACKGROUND],
         backtrace_frame_id:             [GRAY4, BACKGROUND],
         backtrace_frame_id_highlighted: [GREEN, BACKGROUND],
-        backtrace_location:             [GRAY4, BACKGROUND],
+        backtrace_location:             [GRAY5, BACKGROUND],
         backtrace_class_label:          [BLUE, BACKGROUND],
-        backtrace_method_label:         [GRAY5, BACKGROUND],
+        backtrace_method_label:         [WHITE, BACKGROUND],
         variable_mark:                  [GRAY4, BACKGROUND],
         variable_mark_inline:           [GREEN, BACKGROUND],
         variable_loc:                   [PURPLE, BACKGROUND],
         variable_ins:                   [PURPLE, BACKGROUND],
         variable_con:                   [ORANGE, BACKGROUND],
-        variable_inspection:            [GRAY4, BACKGROUND],
-        variable_size:                  [GRAY4, BACKGROUND],
-        variable_separator:             [GRAY5, BACKGROUND],
+        variable_inspection:            [GRAY5, BACKGROUND],
+        variable_size:                  [GRAY5, BACKGROUND],
+        variable_separator:             [WHITE, BACKGROUND],
         source_line_mark:               [GREEN, BACKGROUND],
         source_lineno:                  [GRAY4, BACKGROUND],
         source_token_keyword:           [BLUE, BACKGROUND],
