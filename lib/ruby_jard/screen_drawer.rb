31de5e228304f34e2e910e3ef464d15210d15d12
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-07-27 00:07:09 +0700

    Strim down screen drawer to draw from screen's rendered window only

--- lib/ruby_jard/screen_drawer.rb
+++ lib/ruby_jard/screen_drawer.rb
@@ -1,97 +1,22 @@
 # frozen_string_literal: true
 
 module RubyJard
-  ##
-  # Draw a screen and its rows into the output interface.
   class ScreenDrawer
-    attr_reader :output
-
-    ELLIPSIS = ' »'
-
-    def initialize(output:, screen:)
+    def initialize(output:, screen:, color_scheme:)
       @output = output
-      @color_decorator = RubyJard::Decorators::ColorDecorator.new(screen.color_scheme)
-      @width = screen.width
-      @height = screen.height
-      @x = screen.x
-      @y = screen.y
-      @original_x = screen.x
-      @original_y = screen.y
-      @rows = screen.rows
+      @screen = screen
+      @color_decorator = RubyJard::Decorators::ColorDecorator.new(color_scheme)
     end
 
     def draw
-      # Draw background
-      (@y..@y + @height - 1).each do |y|
-        RubyJard::Console.move_to(@output, @x, y)
-        @output.print @color_decorator.decorate(:background, ' ' * @width)
+      @screen.window.each_with_index do |line, index|
+        RubyJard::Console.move_to(@output, @screen.layout.x, @screen.layout.y + index)
+        @output.print line
       end
-
-      @original_x = @x
-      @rows.each do |row|
-        draw_columns(row, row.columns)
-        @y += 1
-        @x = @original_x
+      (@screen.window.length..@screen.layout.height - 1).each do |index|
+        RubyJard::Console.move_to(@output, @screen.layout.x, @screen.layout.y + index)
+        @output.print @color_decorator.decorate(:background, ' ' * @screen.layout.width)
       end
-    end
-
-    private
-
-    def draw_columns(row, columns)
-      columns.each do |column|
-        width = 0
-        lines = 1
-        column_content_width = column.width - column.margin_left - column.margin_right
-        @x += column.margin_left
-        RubyJard::Console.move_to(@output, @x, @y)
-
-        column.spans.each do |span|
-          line_content = span.content
-
-          until line_content.nil? || line_content.empty?
-            if column_content_width - width <= 0
-              width = 0
-              lines += 1
-              @y += 1
-              RubyJard::Console.move_to(@output, @x, @y)
-            end
-            drawing_content = line_content[0..column_content_width - width - 1]
-            line_content = line_content[column_content_width - width..-1]
-            width += drawing_content.length
-
-            if !row.line_limit.nil? && lines >= row.line_limit && !line_content.nil? && !line_content.empty?
-              drawing_content[drawing_content.length - ELLIPSIS.length..-1] = ELLIPSIS
-              print_with_style(span.styles, drawing_content)
-              return
-            else
-              print_with_style(span.styles, drawing_content)
-            end
-          end
-        end
-        @x += column_content_width + column.margin_right
-      end
-    end
-
-    def print_with_style(styles, drawing_content)
-      # TODO: currently, only row overflow is detected. Definitely should handle column overflow
-      return if @y < @original_y || @y > @original_y + @height - 1
-
-      @output.print @color_decorator.decorate(styles[:element], drawing_content)
-    end
-
-    def default_frame_styles
-      {
-        style: {
-          fg: :white
-        },
-        border: {
-          bottom_left: false,
-          bottom_right: false,
-          bottom: false,
-          left: :line,
-          right: false
-        }
-      }
     end
   end
 end
