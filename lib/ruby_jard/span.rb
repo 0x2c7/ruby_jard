31d9b266cb3d9cc7d8d6562025c10532109291fd
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-07-27 14:12:10 +0700

    Handle span empty content

--- lib/ruby_jard/span.rb
+++ lib/ruby_jard/span.rb
@@ -7,11 +7,13 @@ module RubyJard
     attr_accessor :content, :content_length, :styles
 
     def initialize(content: '', content_length: nil, margin_left: 0, margin_right: 0, styles: [])
-      content = ' ' * margin_left + content if margin_left > 0
-      content += ' ' * margin_right if margin_right > 0
+      if !content.nil? && !content.empty?
+        content = ' ' * margin_left + content if margin_left > 0
+        content += ' ' * margin_right if margin_right > 0
+      end
 
-      @content = content
-      @content_length = content_length || content.length
+      @content = content.to_s
+      @content_length = content_length || @content.length
       @styles = styles
     end
   end
