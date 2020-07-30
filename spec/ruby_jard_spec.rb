41cd723d07f364ea529644e1959446c282abd912
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-07-30 13:22:43 +0700

    Remove redundant test

--- spec/ruby_jard_spec.rb
+++ spec/ruby_jard_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RubyJard do
   it 'has a version number' do
     expect(RubyJard::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(true).to eq(true)
-  end
 end
