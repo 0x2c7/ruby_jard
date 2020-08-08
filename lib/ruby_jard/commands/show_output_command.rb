687e479ffe4f1b128575c25f9b4e5bccb6d0b1b7
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-08-08 14:49:10 +0700

    Implement show-output command

---
+++ lib/ruby_jard/commands/show_output_command.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module RubyJard
+  module Commands
+    # Command used to explore stacktrace.
+    class ShowOutputCommand < Pry::ClassCommand
+      group 'RubyJard'
+      description 'Show all current program output'
+
+      match 'show-output'
+
+      banner <<-BANNER
+        Usage: show-output
+      BANNER
+
+      def self.output_storage
+        RubyJard::ScreenManager.instance.output_storage
+      end
+
+      def process
+        pry_instance.pager.open do |pager|
+          self.class.output_storage.rewind
+          pager.write self.class.output_storage.read until self.class.output_storage.eof?
+        end
+      end
+    end
+  end
+end
+
+Pry::Commands.add_command(RubyJard::Commands::ShowOutputCommand)
--- lib/ruby_jard/repl_processor.rb
+++ lib/ruby_jard/repl_processor.rb
@@ -11,6 +11,7 @@ require 'ruby_jard/commands/step_out_command'
 require 'ruby_jard/commands/frame_command'
 require 'ruby_jard/commands/list_command'
 require 'ruby_jard/commands/color_scheme_command'
+require 'ruby_jard/commands/show_output_command'
 
 module RubyJard
   ##
