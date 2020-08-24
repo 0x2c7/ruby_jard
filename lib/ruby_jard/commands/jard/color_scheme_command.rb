bca264fc1a879cb427be06874a959a3b3857ac7f
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-08-24 21:25:56 +0700

    Improve color-scheme -l command

--- lib/ruby_jard/commands/jard/color_scheme_command.rb
+++ lib/ruby_jard/commands/jard/color_scheme_command.rb
@@ -4,6 +4,8 @@ module RubyJard
   module Commands
     # Command used to explore stacktrace.
     class ColorSchemeCommand < Pry::ClassCommand
+      include RubyJard::Commands::ColorHelpers
+
       group 'RubyJard'
       description 'Control the color scheme used in Jard'
 
@@ -29,23 +31,49 @@ module RubyJard
           if @color_schemes.names.empty?
             pry_instance.output.puts 'No loaded color schemes'
           else
-            pry_instance.output.puts @color_schemes.names.join("\n")
+            print_color_schemes
           end
         else
           color_scheme = args.first.to_s.strip
           if color_scheme.empty?
             raise Pry::CommandError,
-                  'You must provide a color scheme name. Please use `color-scheme -l` to list all color schemes.'
+                  'You must provide a color scheme name. '\
+                  "Please use `#{highlight('jard color-scheme -l')}` to list all color schemes."
           end
 
           if @color_schemes[color_scheme].nil?
             raise Pry::CommandError,
-                  "Color scheme `#{color_scheme}` not found. Please use `color-scheme -l` to list all color schemes."
+                  "Color scheme `#{secondary(color_scheme)}` not found. "\
+                  "Please use `#{highlight('jard color-scheme -l')}` to list all color schemes."
           end
 
           @config.color_scheme = color_scheme
           RubyJard::ControlFlow.dispatch(:list)
         end
+      end
+
+      private
+
+      def print_color_schemes
+        pry_instance.output.puts
+        pry_instance.output.puts highlight("#{@color_schemes.names.length} available color schemes")
+        pry_instance.output.puts
+        padding = @color_schemes.names.map(&:length).max
+        @color_schemes.names.each do |name|
+          scheme = @color_schemes[name]
+          decorator = RubyJard::Decorators::ColorDecorator.new(scheme.new)
+          if scheme.const_defined?(:STYLES)
+            pallete = scheme.const_get(:STYLES).keys.map do |style|
+              decorator.decorate(style, '⬤ ')
+            end.join
+            pry_instance.output.puts "#{name.ljust(padding)} | #{pallete}"
+            pry_instance.output.puts
+          else
+            pry_instance.output.puts name
+          end
+        end
+
+        pry_instance.output.puts
       end
     end
   end
