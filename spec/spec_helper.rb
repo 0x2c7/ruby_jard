56a794613a79619bd7341b5907d0bd6260f93f16
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-08-26 22:39:42 +0700

    Don't need to kill tmux server whenever a test fails anymore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,20 +31,6 @@ RSpec.configure do |config|
     end
   end
 
-  config.retry_callback = proc do
-    if ENV['CI']
-      puts '==== Tmux ===='
-      puts 'Info:'
-      puts `tmux info`
-      puts 'Restart Tmux...'
-      `tmux kill-server`
-      `tmux start-server`
-      `tmux new-session -t dummy -d`
-      `ruby spec/wait_for_tmux.rb`
-      puts '==== End Tmux ===='
-    end
-  end
-
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
