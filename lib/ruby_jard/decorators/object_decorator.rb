d54480767f9e71e031a0066ad6fe1fe8524e1e4b
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-08-30 21:58:30 +0700

    Enhance look and feel of object inspection

--- lib/ruby_jard/decorators/object_decorator.rb
+++ lib/ruby_jard/decorators/object_decorator.rb
@@ -2,7 +2,7 @@
 
 module RubyJard
   class ObjectDecorator
-    RAW_INSPECTION_PATTERN = /#<(.*)(:0x[0-9]+.*)>/i.freeze
+    DEFAULT_INSPECTION_PATTERN = /#<(.*:0x[0-9a-z]+)(.*)>/i.freeze
 
     def initialize(general_decorator)
       @general_decorator = general_decorator
@@ -10,40 +10,18 @@ module RubyJard
     end
 
     def decorate_singleline(variable, line_limit:)
-      raw_inspection = inspect_object(variable)
-      match = raw_inspection.match(RAW_INSPECTION_PATTERN)
-      if match
-        detail =
-          if match[2].length < line_limit - match[1].length - 3
-            match[2]
-          else
-            match[2][0..line_limit - match[1].length - 4] + '…'
-          end
-        [
-          RubyJard::Span.new(content: '#<', styles: :text_secondary),
-          RubyJard::Span.new(content: match[1], styles: :text_secondary),
-          RubyJard::Span.new(content: detail, styles: :text_secondary),
-          RubyJard::Span.new(content: '>', styles: :text_secondary)
-        ]
-      elsif raw_inspection.length <= line_limit
-        [
-          RubyJard::Span.new(
-            content: raw_inspection[0..line_limit],
-            styles: :text_secondary
-          )
-        ]
+      if native_inspect?(variable)
+        decorate_native_inspection(variable, line_limit: line_limit)
       else
-        [
-          RubyJard::Span.new(
-            content: raw_inspection[0..line_limit - 3] + '…>',
-            styles: :text_secondary
-          )
-        ]
+        decorate_custom_inspection(variable, line_limit: line_limit)
       end
     end
 
     def decorate_multiline(variable, first_line_limit:, lines:, line_limit:)
-      spans = [decorate_singleline(variable, line_limit: first_line_limit)]
+      singleline = decorate_singleline(variable, line_limit: first_line_limit)
+      return [singleline] if singleline.map(&:content_length).sum < line_limit
+
+      spans = [excerpt(variable.to_s, line_limit: first_line_limit)]
 
       return spans if !variable.respond_to?(:instance_variables) ||
                       !variable.respond_to?(:instance_variable_get)
@@ -73,12 +51,77 @@ module RubyJard
 
     private
 
-    def inspect_object(variable)
-      if Kernel.method(:method).unbind.bind(variable).call(:inspect).owner == ::Kernel
-        variable.to_s
+    def native_inspect?(variable)
+      Kernel.method(:method).unbind.bind(variable).call(:inspect).owner == ::Kernel
+    end
+
+    def decorate_native_inspection(variable, line_limit:)
+      raw_inspection = variable.to_s
+      match = raw_inspection.match(DEFAULT_INSPECTION_PATTERN)
+
+      if match
+        padding = variable.instance_variables.empty? ? 0 : 1
+        spans = [
+          RubyJard::Span.new(content: '#<', styles: :text_secondary),
+          RubyJard::Span.new(content: match[1], margin_right: padding, styles: :text_secondary)
+        ]
+        spans += @attributes_decorator.inline_pairs(
+          variable.instance_variables.each_with_index, total: variable.instance_variables.length,
+          line_limit: line_limit - spans.map(&:content_length).sum - 1,
+          process_key: false, value_proc: ->(key) { variable.instance_variable_get(key) }
+        )
+        spans << RubyJard::Span.new(content: '>', styles: :text_secondary)
+        spans
+      elsif raw_inspection.length <= line_limit
+        [
+          RubyJard::Span.new(
+            content: raw_inspection[0..line_limit],
+            styles: :text_secondary
+          )
+        ]
       else
-        # Respect custom inspect implementation
-        variable.inspect
+        [
+          RubyJard::Span.new(
+            content: raw_inspection[0..line_limit - 3] + '…>',
+            styles: :text_secondary
+          )
+        ]
+      end
+    end
+
+    def decorate_custom_inspection(variable, line_limit:)
+      excerpt(variable.inspect, line_limit: line_limit)
+    end
+
+    def excerpt(raw_inspection, line_limit:)
+      match = raw_inspection.match(DEFAULT_INSPECTION_PATTERN)
+      if match
+        detail =
+          if match[2].length < line_limit - match[1].length - 3
+            match[2]
+          else
+            match[2][0..line_limit - match[1].length - 4] + '…'
+          end
+        [
+          RubyJard::Span.new(content: '#<', styles: :text_secondary),
+          RubyJard::Span.new(content: match[1], styles: :text_secondary),
+          RubyJard::Span.new(content: detail, styles: :text_secondary),
+          RubyJard::Span.new(content: '>', styles: :text_secondary)
+        ]
+      elsif raw_inspection.length <= line_limit
+        [
+          RubyJard::Span.new(
+            content: raw_inspection[0..line_limit],
+            styles: :text_secondary
+          )
+        ]
+      else
+        [
+          RubyJard::Span.new(
+            content: raw_inspection[0..line_limit - 3] + '…>',
+            styles: :text_secondary
+          )
+        ]
       end
     end
   end
