97b559932e1c581916f8c619781ab1ab26469d99
Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-09-15 10:55:42 +0700

    Tests to cover backward compatibilities with pry and byebug

---
+++ spec/examples/byebug_example.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'ruby_jard'
+
+class SubCalculator
+  def calculate(d)
+    d1 = d * 2
+    d2 = d * 2
+    d1 + d2
+  end
+end
+
+class Calculator
+  def calculate(a, b, c)
+    d = a + b
+    byebug
+    e = SubCalculator.new.calculate(d)
+    byebug
+    e + c
+  end
+end
+
+Calculator.new.calculate(1, 2, 3)
