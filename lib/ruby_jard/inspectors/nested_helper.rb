d6d404b3ffd132fd1a8ffa70597500193b8d4afa
Quang-Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-10-05 17:16:30 +0700

    Clean up inspectors

--- lib/ruby_jard/inspectors/nested_helper.rb
+++ lib/ruby_jard/inspectors/nested_helper.rb
@@ -20,19 +20,21 @@ module RubyJard
         item_limit = total == 0 ? 0 : line_limit_for_pair(depth, line_limit / total)
 
         enum.each do |(key, value), index|
-          key_inspection = inspect_nested_key(key, item_limit, process_key: process_key, depth: depth)
+          row << text_primary(', ') if index > 0
+
+          key_inspection = inspect_nested_key(
+            key,
+            line_limit: item_limit, process_key: process_key, depth: depth
+          )
           value_inspection = @base.inline(
             value_proc.nil? ? value : value_proc.call(key),
             line_limit: line_limit_for_pair(depth, item_limit - key_inspection.content_length), depth: depth
           )
 
-          row << text_primary(', ') if index > 0
-
           if row.content_length + key_inspection.content_length + value_inspection.content_length + 6 > line_limit
             row << sym_ellipsis
             break
           end
-
           row << key_inspection
           row << sym_arrow
           row << value_inspection
@@ -45,20 +47,15 @@ module RubyJard
         return SimpleRow.new(sym_ellipsis) if too_deep?(depth, line_limit)
 
         row = SimpleRow.new(sym_bullet)
-
-        key_inspection = inspect_nested_key(
+        row << inspect_nested_key(
           key,
-          line_limit - sym_bullet.content_length,
+          line_limit: line_limit - sym_bullet.content_length,
           process_key: process_key, depth: depth
         )
-
-        row << key_inspection
         row << sym_arrow
-        value_inspection = @base.inline(
+        row << @base.inline(
           value, line_limit: line_limit_for_pair(depth, line_limit - row.content_length), depth: depth
         )
-
-        row << value_inspection
       end
 
       def inline_values(enum, total:, line_limit:, depth: 0)
@@ -68,17 +65,16 @@ module RubyJard
         item_limit = total == 0 ? 0 : line_limit_for_value(line_limit / total)
 
         enum.each do |value, index|
+          row << text_primary(', ') if index > 0
+
           value_inspection = @base.inline(
             value, line_limit: line_limit_for_value(item_limit), depth: depth
           )
-
-          row << text_primary(', ') if index > 0
 
           if row.content_length + value_inspection.content_length + 2 > line_limit
             row << sym_ellipsis
             break
           end
-
           row << value_inspection
         end
 
@@ -89,17 +85,16 @@ module RubyJard
         return [sym_ellipsis] if too_deep?(depth, line_limit)
 
         row = SimpleRow.new(sym_bullet)
-        value_inspection = @base.inline(
+        row << @base.inline(
           value, line_limit: line_limit_for_value(line_limit - row.content_length), depth: depth
         )
-        row << value_inspection
       end
 
       private
 
-      def inspect_nested_key(key, item_limit, process_key:, depth: 0)
+      def inspect_nested_key(key, line_limit:, process_key:, depth: 0)
         if process_key
-          @base.inline(key, line_limit: item_limit, depth: depth)
+          @base.inline(key, line_limit: line_limit, depth: depth)
         else
           SimpleRow.new(text_primary(key.to_s))
         end
