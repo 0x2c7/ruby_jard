c20570e388e8b23d74398caf964d998ef59ec3c8
Quang-Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-10-05 13:37:58 +0700

    Differentiate row and rows returned from decorator

--- spec/helpers/decorator_helper.rb
+++ spec/helpers/decorator_helper.rb
@@ -1,16 +1,41 @@
 # frozen_string_literal: true
 
-RSpec::Matchers.define :match_spans do |expected|
+RSpec::Matchers.define :match_row do |expected|
   match do |actual|
+    raise 'Row expected' unless actual.is_a?(RubyJard::Row)
+
     @expected = expected.strip
-    @actual =
-      if actual.is_a?(Array) && actual[0].is_a?(Array)
-        actual.map { |line| line.map(&:content).join }.join("\n")
-      elsif actual.is_a?(Array)
-        actual.map(&:content).join
-      else
-        actual.content
-      end
+    @actual = actual.spans.map(&:content).join
+    # Mask object address
+    @actual.gsub!(/0x[0-9a-z]{10,}/i) { |found| '?' * found.length }
+    @actual == @expected
+  end
+
+  failure_message do |actual|
+    <<~SCREEN
+      Expected:
+      ###
+      #{expected}
+      ###
+
+      Actual:
+      ###
+      #{actual}
+      ###
+    SCREEN
+  end
+
+  diffable
+  attr_reader :actual, :expected
+end
+
+RSpec::Matchers.define :match_rows do |expected|
+  match do |actual|
+    raise 'Array of row expected' unless actual.is_a?(Array)
+    raise 'Array of row expected' if !actual.empty? && !actual.all? { |r| r.is_a?(RubyJard::Row) }
+
+    @expected = expected.strip
+    @actual = actual.map { |row| row.spans.map(&:content).join }.join("\n")
     # Mask object address
     @actual.gsub!(/0x[0-9a-z]{10,}/i) { |found| '?' * found.length }
     @actual == @expected
