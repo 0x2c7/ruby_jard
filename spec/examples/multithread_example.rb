fe83510b105fb3037b06dbec97f976986f1ad555
Quang-Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-10-13 22:59:45 +0700

    Design a better multithread test

--- spec/examples/multithread_example.rb
+++ spec/examples/multithread_example.rb
@@ -1,17 +1,29 @@
 # frozen_string_literal: true
 require 'ruby_jard'
 
-def process(index)
-  a = index
-  b = Thread.current[:index]
-  jard if index % 2 == 1
-end
+class Test
+  def initialize
+    @index = 1
+  end
 
-threads = (1..4).map do |index|
-  Thread.new do
-    Thread.current[:index] = index
-    process(index)
-    sleep 1
+  def process(index)
+    a = index
+    b = Thread.current[:index]
+    sleep 0.1 until @index >= index
+    jard
+  end
+
+  def start
+    threads = (1..4).map do |index|
+      Thread.new do
+        Thread.current[:index] = index
+        process(index)
+      end
+    end
+    threads.map(&:join)
   end
 end
-threads.map(&:join)
+
+Test.new.start
+jard
+sleep 0
--- spec/integration/multithread/multithread_spec.rb
+++ spec/integration/multithread/multithread_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Debugging multi-threads', integration: true do
-  let(:work_dir) { File.join(RSPEC_ROOT, '/integration/threads') }
+  let(:work_dir) { File.join(RSPEC_ROOT, '/integration/multithread') }
 
   context 'when two threads are attaching' do
     it 'attaches one thread, and hold another thread' do
@@ -12,6 +12,7 @@ RSpec.describe 'Debugging multi-threads', integration: true do
       )
       test.start
       test.assert_screen
+      test.send_keys('@index = 2', :Enter)
       test.send_keys('a', :Enter)
       test.send_keys('b', :Enter)
       test.send_keys('a + b', :Enter)
@@ -21,6 +22,12 @@ RSpec.describe 'Debugging multi-threads', integration: true do
       test.send_keys('a', :Enter)
       test.send_keys('b', :Enter)
       test.send_keys('a + b', :Enter)
+      test.assert_screen
+      test.send_keys('@index = 4', :Enter)
+      test.send_keys('continue', :Enter)
+      test.send_keys('continue', :Enter)
+      test.send_keys('continue', :Enter)
+      sleep 1
       test.assert_screen
     ensure
       test.stop
