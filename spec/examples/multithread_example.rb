72fd29dbbc95efb01fed817873520f1e8272f967
Quang-Minh Nguyen <nguyenquangminh0711@gmail.com>
2020-10-13 15:23:41 +0700

    Fix unstable test

--- spec/examples/multithread_example.rb
+++ spec/examples/multithread_example.rb
@@ -4,14 +4,14 @@ require 'ruby_jard'
 def process(index)
   a = index
   b = Thread.current[:index]
-  jard if index % 2 == 0
+  jard if index % 2 == 1
 end
 
 threads = (1..4).map do |index|
   Thread.new do
     Thread.current[:index] = index
     process(index)
-    sleep 0.5
+    sleep 1
   end
 end
 threads.map(&:join)
